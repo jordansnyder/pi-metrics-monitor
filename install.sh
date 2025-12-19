7a493f5fe764ac3842669f14260dcf78519d9bb7
Jordan Snyder <jordan.snyder@gmail.com>
2025-12-18 19:46:44 -0700

    fixed bug in install.sh again

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
-#/bin/bash
-# Install pip
-sudo apt-get install pip3
+#!/bin/bash
 
 # Install prometheus-client
+echo "Install Prometheus Client system-wide..."
 sudo pip3 install prometheus-client
 
 # Install the python3-systemd package for Journal integration
+echo "Install systemd package for syslog integration"
 sudo apt-get install python3-systemd
 
+echo "Create directory for scripts..."
 sudo mkdir /opt/pi-metrics-service
-sudo mv $(pwd)/metrics_monitor.py /opt/pi-metrics-service/
-sudo mv $(pwd)/pi-metrics-monitor.service /etc/systemd/system/
+sudo cp $(pwd)/metrics_monitor.py /opt/pi-metrics-service/
+sudo cp $(pwd)/pi-metrics-monitor.service /etc/systemd/system/
 
 # Enable and start the sensor-metrics.service
 sudo systemctl enable pi-metrics-monitor.service
