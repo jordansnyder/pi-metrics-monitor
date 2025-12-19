821800f49dda33ccd2e011d14862332a893b8a7d
Jordan Snyder <jordan.snyder@gmail.com>
2025-12-18 19:22:23 -0700

    added service definition file and updated install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+# Install pip
+sudo apt-get install pip3
+
+# Install prometheus-client
+sudo pip3 install prometheus-client
+
+# Install the python3-systemd package for Journal integration
+sudo apt-get install python3-systemd
+
+sudo mkdir /opt/pi-metrics-monitor
+sudo mv $(pwd)/metrics_monitor.py /opt/pi-metrics-monitor/
+sudo mv $(pwd)/pi-metrics-monitor.service /etc/systemd/system/
+
+# Enable and start the sensor-metrics.service
+sudo systemctl enable pi-metrics-monitor.service
+sudo systemctl start pi-metrics-monitor.service
---
+++ metrics_monitor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import logging
+import time
+
+import subprocess
+# Import Gauge and start_http_server from prometheus_client
+from prometheus_client import Gauge, start_http_server
+from systemd.journal import JournalHandler
+
+READ_INTERVAL = 5.0
+
+# Setup logging to the Systemd Journal
+log = logging.getLogger('metrics_monitor')
+log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
+
+# Create Prometheus gauges for humidity and temperature in
+# Celsius and Fahrenheit
+gpuTempGauge = Gauge('gpu_temperature', 'GPU Temperature')
+cpuTempGauge = Gauge('cpu_temperature', 'CPU Temperature')
+
+def read_sensors():
+	try:
+		gpuout = subprocess.run(["vcgencmd", "measure_temp"], capture_output=True)
+		if gpuout.stdout.startswith(b"temp="):
+			part = gpuout.stdout[len("temp="):]
+			if part.endswith(b"'C\n"):
+				gpu = float(part[:len(part) - len("'C\n")])
+
+		cpufile = '/sys/class/thermal/thermal_zone0/temp'
+		with open(cpufile) as file:
+			data = file.read()
+			cpu = int(data) / 1000
+
+		if gpu is not None and cpu is not None:
+			gpuTempGauge.set(gpu)
+			cpuTempGauge.set(cpu)
+			#log.info("GPU Temp: {0:0.1f}*C, CPU Temp: {0:0.1f}*C".format(gpu, cpu))
+	except RuntimeError as e:
+        	# GPIO access may require sudo permissions
+        	# Other RuntimeError exceptions may occur, but
+        	# are common.  Just try again.
+		log.error("RuntimeError: {}".format(e))
+	
+	time.sleep(READ_INTERVAL)
+
+if __name__=='__main__':
+	# Expose metrics
+	metrics_port = 8000
+	start_http_server(metrics_port)
+	print("Serving sensor metrics on :{}".format(metrics_port))
+	log.info("Serving sensor metrics on :{}".format(metrics_port))
+
+	while True:
+		read_sensors()
