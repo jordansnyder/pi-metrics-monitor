ecb4ae224b312687af436960a2923f74a092ece1
Jordan Snyder <jordan.snyder@gmail.com>
2025-12-18 19:44:22 -0700

    fixed bug in install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ sudo pip3 install prometheus-client
 # Install the python3-systemd package for Journal integration
 sudo apt-get install python3-systemd
 
-sudo mkdir /opt/pi-metrics-monitor
-sudo mv $(pwd)/metrics_monitor.py /opt/pi-metrics-monitor/
+sudo mkdir /opt/pi-metrics-service
+sudo mv $(pwd)/metrics_monitor.py /opt/pi-metrics-service/
 sudo mv $(pwd)/pi-metrics-monitor.service /etc/systemd/system/
 
 # Enable and start the sensor-metrics.service
